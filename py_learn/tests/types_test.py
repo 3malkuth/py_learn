9d3ba26800d817ad05d1cdec2a3a27b71a1e761f
3malkuth <sy5t3ms@protonmail.com>
2023-10-17 16:21:00 +0200

    updated

--- py_learn/tests/types_test.py
+++ py_learn/tests/types_test.py
@@ -4,5 +4,8 @@ import pytest
 @pytest.mark.utest
 def test_it_should_confirm_variable_of_type_list():
     num_range: list = list(range(10))
-    assert type(num_range) is list
+    assert type(num_range) is dict
 
+if __name__ ==	"__main__":
+    test_it_should_confirm_variable_of_type_list()
+    
